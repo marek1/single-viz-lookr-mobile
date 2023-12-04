aade04fe126fa9ddb4050a7eda27d6ba21e33086
--global <marek.sonnabend.ext@adevinta.com>
2023-12-04 19:55:29 +0100

    JS-20

--- index.js
+++ index.js
@@ -85,8 +85,9 @@ looker.plugins.visualizations.add({
         element.style.fontSize = config.fontSize;
         element.style.textAlign = "center";
         //element.innerHTML += "<div style='font-size: 2rem;'>" + config.htmlSubtitle + "</div>";
-        element.innerHTML += "<div style='font-size: 2rem;'>" + qFields.dimension_like[0] + "</div>";
-        element.innerHTML += "<div style='font-size: 2rem;'>" + qFields.measure_like + "</div>";
+        element.innerHTML += "<div style='font-size: 2rem;'>l: " + qFields.dimension_like[0].name + "</div>";
+        element.innerHTML += "<div style='font-size: 2rem;'>m: " + qFields.measure_like[0].name + "</div>";
+        element.innerHTML += "<div style='font-size: 2rem;'>do</div>";
 
         doneRendering();
     }
