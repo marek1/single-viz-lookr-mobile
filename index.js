1a410f49eb4ee4105b43c75e26d48d10431350ec
Marek <marek.sonnabend.ext@adevinta.com>
2023-12-05 19:48:07 +0100

    adding table 26

--- index.js
+++ index.js
@@ -158,9 +158,11 @@ looker.plugins.visualizations.add({
         element.innerHTML += "<div style='float: left; width:33%; font-size: 1rem !important'>from Target</div>";
         element.innerHTML += "</div>"
         element.innerHTML += "<div style='width:100%; '>";
-        element.innerHTML += "<div style='float: left; width:33%; font-size: 3rem !important'>" +
-            + yoyValue > 0 ? "<div style='color:green;'> ▲ </div>" : "" +
-            + yoyValue + " %</div>";
+        element.innerHTML += "<div style='float: left; width:33%; font-size: 3rem !important'>";
+        if (yoyValue > 0) {
+            element.innerHTML += "<div style='color:green;'> ▲ </div>";
+        }
+        element.innerHTML += yoyValue + " %</div>";
         element.innerHTML += "<div style='float: left; width:33%; font-size: 3rem !important'></div>";
         element.innerHTML += "<div style='float: left; width:33%; font-size: 3rem !important'></div>";
         element.innerHTML += "</div>"
