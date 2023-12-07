0e39d17644dc7fc6240b69114a118dfd6f8eba06
Marek <marek.sonnabend.ext@adevinta.com>
2023-12-07 12:19:30 +0100

    custom format of value 15

--- index.js
+++ index.js
@@ -372,7 +372,7 @@ looker.plugins.visualizations.add({
             if (config.formatValue !== "none") {
                 console.log('config.formatValue : ', config.formatValue);
                 console.log('htmlForCell : ', htmlForCell);
-                htmlForCell = htmlForCell.toLocaleString(
+                htmlForCell = toString(htmlForCell).toLocaleString(
                     config.formatValue,
                     { minimumFractionDigits: config.formatDigits }
                 );
